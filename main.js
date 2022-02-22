8d18d66876e3cc23464c9b8dffb71f13f5380e1d
tangxve <11183504@qq.com>
2022-02-22 19:44:37 +0800

    feat: 添加 小时钟

--- main.js
+++ main.js
@@ -1,45 +1,53 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, ipcMain, Notification } = require('electron')
 const path = require('path')
 
+let mainWindow
 // 01、创建一个窗口
 // 02：让窗口加载一个界面，这个界面用web 技术实现，这个界面运行在渲染进程中
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+  mainWindow = new BrowserWindow({
+    width: 300,
+    height: 300,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      // preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
     }
   })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
+  handleIPC()
+
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
+function handleIPC() {
+  ipcMain.handle('work-notification', async function () {
+    const res = await new Promise((resolve, reject) => {
+      const notification = new Notification({
+        title: '任务结束',
+        body: '是否开始休息',
+        actions: [{ text: '开始休息', type: 'button' }],
+        closeButtonText: '继续工作',
+      })
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+      notification.show()
+      notification.on('action', () => {
+        resolve('rest')
+      })
+      notification.on('close', () => {
+        resolve('work')
+      })
+    })
+    return res
   })
-})
+}
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+app.on('ready', () => {
+  createWindow()
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
